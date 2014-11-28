2451281635f3b7a002de34a993063be742863494
yamadapc <tacla.yamada@gmail.com>
2014-11-28 09:07:06 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+tmux-pane-rel
+=============
+A helper for quickly resizing tmux panes to a percentage of a session's total
+width/height
+
+License
+-------
+This code is licensed under the MIT license for Pedro Tacla Yamada. Please refer
+to the [LICENSE](/LICENSE) file for more information.
---
+++ source/app.d
@@ -0,0 +1,54 @@
+import std.algorithm;
+import std.conv : to;
+import std.getopt : getopt;
+import std.process : execute;
+import std.stdio : writeln;
+import std.string : split;
+
+static immutable string usage = "Usage: tmux-pane-rel [options] <percentage>";
+
+enum axis { x, y }
+
+int main(string[] args) {
+  if(args.length == 1) {
+    writeln(usage);
+  }
+
+  bool help;
+  axis ax;
+
+  getopt(
+    args,
+    "axis|a", &ax,
+    "help|h", &help
+  );
+
+  if(help || args.length < 2) {
+    writeln(usage);
+    return 1;
+  }
+
+  resizeTmuxPane(ax, args[args.length - 1].to!int);
+
+  return 0;
+}
+
+int getTerminalWidth() {
+  return execute(["tmux", "list-windows", "-F", "#{session_width}"])
+    .output
+    .split("\n")[0]
+    .to!int;
+}
+
+int getTerminalHeight() {
+  return execute(["tmux", "list-windows", "-F", "#{session_height}"])
+    .output
+    .split("\n")[0]
+    .to!int;
+}
+
+void resizeTmuxPane(axis ax, int percentage) {
+  auto wid = ax == axis.x ? getTerminalWidth() : getTerminalHeight();
+  auto absValue = ((percentage.to!float / 100) * wid.to!float).to!int;
+  execute(["tmux", "resize-pane", "-" ~ ax.to!string, absValue.to!string]);
+}
